0c5627e63fea5979fec88444acd17d43c2724d4a
Praveen <praveenskp@github.com>
2017-12-15 08:05:44 +1100

    Removed unused dependency

--- src/main/java/com/prav/config/SecurityConfig.java
+++ src/main/java/com/prav/config/SecurityConfig.java
@@ -11,33 +11,31 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
 	@Value("${secure.user}")
 	private String username;
 
 	@Value("${secure.pass}")
 	private String password;
-	
+
 	@Value("${secure.admin}")
 	private String adminuser;
-	
-	protected void configure(AuthenticationManagerBuilder auth)
-			throws Exception {
-		auth.inMemoryAuthentication().withUser(username).password(password)
-				.roles("USER").and().withUser(adminuser).password(password)
-				.roles("USER", "ADMIN");
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser(username).password(password).roles("USER").and().withUser(adminuser)
+				.password(password).roles("USER", "ADMIN");
 	}
 
-	
+	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.httpBasic().and().authorizeRequests().antMatchers("/gcd/**")
-				.hasRole("USER").antMatchers("/**").hasRole("ADMIN").and()
-				.csrf().disable().headers().frameOptions().disable();
+		http.httpBasic().and().authorizeRequests().antMatchers("/gcd/**").hasRole("USER").antMatchers("/**")
+				.hasRole("ADMIN").and().csrf().disable().headers().frameOptions().disable();
 	}
-	
+
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-	    web.ignoring().antMatchers("/soapservice/**");
+		web.ignoring().antMatchers("/soapservice/**");
 	}
 
 }
